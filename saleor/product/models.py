5d78dee14ba54ac276085285f11130af9264d366
Jakub Draganek <jakub.draganek@gmail.com>
2019-07-19 11:19:08 +0200

    Switch ProductType to new mixin

--- saleor/product/models.py
+++ saleor/product/models.py
@@ -21,8 +21,15 @@ from prices import MoneyRange
 from text_unidecode import unidecode
 from versatileimagefield.fields import PPOIField, VersatileImageField
 
+from saleor.core.models import ModelWithMetadata
+
 from ..core.exceptions import InsufficientStock
-from ..core.models import PublishableModel, PublishedQuerySet, SortableModel
+from ..core.models import (
+    ModelWithMetadata,
+    PublishableModel,
+    PublishedQuerySet,
+    SortableModel,
+)
 from ..core.utils import build_absolute_uri
 from ..core.utils.json_serializer import CustomJsonEncoder
 from ..core.utils.translations import TranslationProxy
@@ -83,7 +90,7 @@ class CategoryTranslation(SeoModelTranslation):
         )
 
 
-class ProductType(models.Model):
+class ProductType(ModelWithMetadata):
     name = models.CharField(max_length=128)
     has_variants = models.BooleanField(default=True)
     is_shipping_required = models.BooleanField(default=True)
@@ -91,7 +98,6 @@ class ProductType(models.Model):
     weight = MeasurementField(
         measurement=Weight, unit_choices=WeightUnits.CHOICES, default=zero_weight
     )
-    meta = JSONField(blank=True, null=True, default=dict, encoder=CustomJsonEncoder)
 
     class Meta:
         app_label = "product"
